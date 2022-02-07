01947d8fb92497bcdf494c1620de73b2f2afdea0
KenshinGH <99211287+KenshinGH@users.noreply.github.com>
2022-02-07 21:18:32 +0200

    Add files via upload

---
+++ my1.py
@@ -0,0 +1,12 @@
+n, s = int(input()), input()
+
+for i in s:
+
+	index = ord(i) - n
+
+	if index < 97:
+		index = (26 - n) + (ord(i) - 96) + 96
+
+	if index >= 97:
+		print(chr(index), end = '')
+print()
